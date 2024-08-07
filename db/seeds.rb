f164ec4930125f02cb4ac75fedcd95897d186727
Carloz <carllozada50@gmail.com>
2024-08-07 20:08:21 +0800

    Committing to V-3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,20 @@ admin_role = Role.find_or_create_by(name: 'admin')
 user_role = Role.find_or_create_by(name: 'user')
 
 # Create default admin user
-admin1 = User.find_or_create_by(email: 'admintrisha@manga.com') do |user|
+admin1 = User.find_or_create_by(email: 'admincarloz@manga.com') do |user|
   user.password = 'mangas'
   user.password_confirmation = 'mangas'
   user.add_role :admin
 end
 
-# Create second admin user
-admin2 = User.find_or_create_by(email: 'admincarloz@manga.com') do |user|
-  user.password = 'mangas'
-  user.password_confirmation = 'mangas'
+admin2 = User.find_or_create_by(email: 'admintrisha@manga.com') do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
   user.add_role :admin
+end
+
+reader1 = User.find_or_create_by(email: 'user1@email.com') do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.add_role :user
 end
